66a857b4099a2ad8ff792b8eddf1186c1f34fa48
Kyle Maxwell <kyle.maxwell@reddit.com>
2024-11-15 13:09:41 -0800

    updated to devvit-hub 0.0.24

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { BasicGameServer } from "devvit-hub";
 
 const server = new BasicGameServer("My Chat app");
 
+server.onPostCreated = async (post) => {
+  console.log("Post created", post);
+  await server.setInterval(`update-${post.postId}`, 1000);
+};
+
 export default server.build();
--- webroot/index.js
+++ webroot/index.js
@@ -3,17 +3,12 @@ const input = document.getElementById("messageInput");
 const button = document.getElementById("sendButton");
 
 function addMessage(text, isSent) {
-  if (text === latest) {
-    return;
-  }
   const messageDiv = document.createElement("div");
   messageDiv.className = `message ${isSent ? "sent" : "received"}`;
   messageDiv.textContent = text;
   chatDiv.appendChild(messageDiv);
   chatDiv.scrollTop = chatDiv.scrollHeight;
 }
-
-let latest = "";
 
 function sendMessage() {
   const text = input.value.trim();
@@ -22,7 +17,6 @@ function sendMessage() {
     addMessage(text, true);
     input.value = "";
   }
-  latest = text;
 }
 
 button.addEventListener("click", sendMessage);
@@ -33,12 +27,21 @@ input.addEventListener("keypress", (e) => {
   }
 });
 
+window.screenId = "";
+
 window.addEventListener("message", (event) => {
   console.log("got", event);
-  const data = event.data.data.message.data;
-  if (data.joined) {
-    addMessage(`User joined: ${JSON.stringify(event.data.data)}`, false);
+  const data = event.data.data.message.data || event.data.data.message;
+  if (data instanceof Object) {
+    if (data.ready) {
+      console.log("setting screen id", data.ready.screenId);
+      window.screenId = data.ready.screenId;
+    }
+    addMessage(JSON.stringify(data), false);
     return;
   }
-  addMessage(data, false);
+  console.log(event.data.data.message.from.screenId, window.screenId);
+  if (event.data.data.message.from.screenId !== window.screenId) {
+    addMessage(data, false);
+  }
 });
